f7627104c55a1ad425d8755b9faf10bccfadf571
Muhammad Fariz <mfarix730@gmail.com>
2025-07-13 14:26:40 +0700

    modif seeder

--- database/seeders/MataKuliahSeeder.php
+++ database/seeders/MataKuliahSeeder.php
@@ -10,20 +10,20 @@ class MataKuliahSeeder extends Seeder
     public function run(): void
     {
         $mataKuliahs = [
-            ['kodeMatkul' => 'MK001', 'namaMatkul' => 'Pemrograman Web', 'jenis' => 'Teori & Praktikum', 'sks' => 3],
-            ['kodeMatkul' => 'MK002', 'namaMatkul' => 'Basis Data', 'jenis' => 'Teori & Praktikum', 'sks' => 3],
-            ['kodeMatkul' => 'MK003', 'namaMatkul' => 'Algoritma dan Pemrograman', 'jenis' => 'Teori & Praktikum', 'sks' => 4],
-            ['kodeMatkul' => 'MK004', 'namaMatkul' => 'Struktur Data', 'jenis' => 'Teori & Praktikum', 'sks' => 3],
-            ['kodeMatkul' => 'MK005', 'namaMatkul' => 'Jaringan Komputer', 'jenis' => 'Teori & Praktikum', 'sks' => 3],
-            ['kodeMatkul' => 'MK006', 'namaMatkul' => 'Sistem Operasi', 'jenis' => 'Teori', 'sks' => 2],
-            ['kodeMatkul' => 'MK007', 'namaMatkul' => 'Pemrograman Mobile', 'jenis' => 'Praktikum', 'sks' => 2],
-            ['kodeMatkul' => 'MK008', 'namaMatkul' => 'Kecerdasan Buatan', 'jenis' => 'Teori', 'sks' => 3],
-            ['kodeMatkul' => 'MK009', 'namaMatkul' => 'Pengembangan Aplikasi', 'jenis' => 'Praktikum', 'sks' => 2],
-            ['kodeMatkul' => 'MK010', 'namaMatkul' => 'Keamanan Sistem', 'jenis' => 'Teori', 'sks' => 2],
+            ['kodeMatkul' => 'MK001', 'namaMatkul' => 'Pemrograman Web', 'jenis' => 'wajib', 'sks' => 3],
+            ['kodeMatkul' => 'MK002', 'namaMatkul' => 'Basis Data', 'jenis' => 'wajib', 'sks' => 3],
+            ['kodeMatkul' => 'MK003', 'namaMatkul' => 'Algoritma dan Pemrograman', 'jenis' => 'wajib', 'sks' => 4],
+            ['kodeMatkul' => 'MK004', 'namaMatkul' => 'Struktur Data', 'jenis' => 'wajib', 'sks' => 3],
+            ['kodeMatkul' => 'MK005', 'namaMatkul' => 'Jaringan Komputer', 'jenis' => 'wajib', 'sks' => 3],
+            ['kodeMatkul' => 'MK006', 'namaMatkul' => 'Sistem Operasi', 'jenis' => 'pilihan', 'sks' => 2],
+            ['kodeMatkul' => 'MK007', 'namaMatkul' => 'Pemrograman Mobile', 'jenis' => 'pilihan', 'sks' => 2],
+            ['kodeMatkul' => 'MK008', 'namaMatkul' => 'Kecerdasan Buatan', 'jenis' => 'pilihan', 'sks' => 3],
+            ['kodeMatkul' => 'MK009', 'namaMatkul' => 'Pengembangan Aplikasi', 'jenis' => 'pilihan', 'sks' => 2],
+            ['kodeMatkul' => 'MK010', 'namaMatkul' => 'Keamanan Sistem', 'jenis' => 'pilihan', 'sks' => 2],
         ];
 
         foreach ($mataKuliahs as $mk) {
             MataKuliah::create($mk);
         }
     }
-} 
+}
